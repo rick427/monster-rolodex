e767ff38babdf79e73651bdd0917c1c51ffae802
rick427 <44168932+rick427@users.noreply.github.com>
2019-07-28 17:00:41 +0100

    Updates

---
+++ precache-manifest.43b808131d72627ac61414f2d64d2199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22e2165b741c2aa63cb10f9353012c4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c0a221ec0893f847ed13",
+    "url": "/monster-rolodex/static/css/main.aaab9940.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0a221ec0893f847ed13",
+    "url": "/monster-rolodex/static/js/main.b757f8b4.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
